90632149a064e70b1e7e251bc007f5414ebf4e7f
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-08-23 19:15:49 +0200

    int to binary

---
+++ binary/int-to-binary.ts
@@ -0,0 +1,10 @@
+export const baseTenToBinary = (n: number): string => {
+  let num = n
+  let binary = (num % 2).toString()
+
+  while (num > 1) {
+    num = num / 2
+    binary = (num % 2) + binary
+  }
+  return binary
+}
